798a6063a726766e3163ec2d1c737ad7678ca92c
tmtuanyd <tmtuanyd@gmail.com>
2024-07-02 23:21:02 +0700

    feat: init app

---
+++ theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mantine/core';
+
+export const theme = createTheme({
+  /* Put your mantine theme override here */
+  breakpoints: {
+    xs: '30em',
+    sm: '48em',
+    md: '920px',
+    lg: '1180px',
+    xl: '90em',
+  },
+});
